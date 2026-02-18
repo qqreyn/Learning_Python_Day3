98b28a99a1498365bd261f9bb23026711d7f7b2f
qqreyn <duch.jan.8@gmail.com>
2026-02-18 15:50:54 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+#Input an int number
+bok_a = int(input("Bok A:"))
+bok_b = int(input("Boka B:"))
+
+print("Pole prostokątu o bokach a =", bok_a, "oraz b =", bok_b, "jest równe:" )
+print(bok_a * bok_b)
