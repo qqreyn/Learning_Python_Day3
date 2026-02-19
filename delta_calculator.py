e165c4730b709801649aff2519941abec666ee7d
qqreyn <duch.jan.8@gmail.com>
2026-02-19 16:02:06 +0100

    Refactor delta calculator for better output
    Added output for the square root of delta and improved formatting.

--- delta_calculator.py
+++ delta_calculator.py
@@ -6,22 +6,22 @@ b = int(input("Enter number b: "))
 c = int(input("Enter number c: "))
 
 delta = (b*b) - 4 * a * c
-print("Delta is equal: ", delta)
 
 if (delta >= 0):
     sqrt_delta = (math.sqrt(delta))
+    print("Delta is equal: √" ,delta, "=", sqrt_delta )
+
+    x1 = (-b - sqrt_delta ) / 2 * a
+    x2 = (-b + sqrt_delta ) / 2 * a
+    x0 = -b / 2 * a
+
+    if (delta > 0):
+        print("Delta > 0, 2 solutions:")
+        print("x1 = ", x1)
+        print("x2 = ", x2)
+    elif (delta == 0):
+        print("Delta == 0, 1 solution")
+        print("x0 = ", x0)
 else:
     print("Delta < 0, no solutions")
     quit()
-
-x1 = (-b - sqrt_delta ) / 2 * a
-x2 = (-b + sqrt_delta ) / 2 * a
-x0 = -b / 2 * a
-
-if (delta > 0):
-    print("Delta > 0, 2 solutions:")
-    print("x1 = ", x1)
-    print("x2 = ", x2)
-elif (delta == 0):
-    print("Delta == 0, 1 solution")
-    print("x0 = ", x0)
